1aac27ce1022d95f5b75a8dcd11620b5b3cff1f2
jusish <ishimwejustin67@gmail.com>
2024-01-24 11:25:46 +0200

    initial commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,6 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   function handleSearch(term: string) {
+    console.log(`Searching...${term}`);
+    
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -29,6 +31,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
