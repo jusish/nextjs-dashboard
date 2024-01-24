8928a1707c207a8fe6e5a84646a01d9211a2cb9f
jusish <ishimwejustin67@gmail.com>
2024-01-24 11:32:45 +0200

    Happy to make this nextjs dashboard 🤣🥳

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,12 +4,14 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term)=>{
     console.log(`Searching...${term}`);
     
     const params = new URLSearchParams(searchParams);
@@ -19,7 +21,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
