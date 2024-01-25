29d4d4a536d7a94352cb7a5307cb36e730383022
jusish <ishimwejustin67@gmail.com>
2024-01-25 07:11:44 +0200

    Happy to make this nextjs dashboard 🤣🥳

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { UpdateInvoice } from './buttons';
 
 export default function EditInvoiceForm({
   invoice,
