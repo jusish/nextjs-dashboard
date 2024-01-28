d5a4a225710eeec46ca4820015c277838430898c
jusish <ishimwejustin67@gmail.com>
2024-01-28 21:22:08 +0200

    new changes

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -81,7 +81,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to delete Invoice');
+  // throw new Error('Failed to delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
